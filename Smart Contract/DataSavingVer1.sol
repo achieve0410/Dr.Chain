2bc591d011ebc0260bd5a5e8e2650302c398764a
jae123123 <40753685+jae123123@users.noreply.github.com>
2019-05-07 17:02:30 +0900

    Update DataSavingVer1.sol

--- Smart Contract/DataSavingVer1.sol
+++ Smart Contract/DataSavingVer1.sol
@@ -23,37 +23,48 @@ library SafeMath {
       assert(c >= _a);
       return c;
   }
-}contract DataStorage{
-    
+}contract UserData{
+    using SafeMath for uint256;
+
+
     struct Info{
         address key;
         uint256 sex;  //ex: Male:0 Female:1
         uint256 birth; //ex: 19940809
+        mapping(uint256=>string) referInfo;
     }
-    
+
     address creator;
     uint256 public total_user;
-    
+
     mapping(address=>Info) public user_list;
-    
+
     modifier onlyCreator{
         require(msg.sender==creator);
         _;
     }
-    
+
     constructor() public{
         creator=msg.sender;
         total_user=0;
     }
-    
+
     function Input_list(address _key, uint256 _sex, uint256 _birth) public onlyCreator {
-        total_user++;
+        total_user=total_user.add(1);
         user_list[_key].key=_key;
         user_list[_key].sex=_sex;
         user_list[_key].birth=_birth;
     }
-    
+
+    function Input_ReferInfo(address _key,uint256 _specNum, string memory _contents) public onlyCreator{
+        user_list[_key].referInfo[_specNum]=_contents;
+    }
+
     function Show_list(address _key)  public view returns(address,uint256,uint256){
         return (user_list[_key].key,user_list[_key].sex,user_list[_key].birth);
+    }
+
+    function Show_User_SpecReferInfo(address _key,uint256 _specNum) public view returns(string memory){
+        return (user_list[_key].referInfo[_specNum]);
     }
 }
