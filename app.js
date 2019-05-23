3e0cd7032aba0867d3870848d0bdefdf68ab0733
whChoi <achieve0410@gmail.com>
2019-05-23 09:50:33 +0900

    commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+
+const user = require('./use_db/user.js');
+const hospital = require('./use_db/hospital.js');
+const records = require('./use_db/records.js');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/user', user);
+app.use('/hospital', hospital);
+app.use('/records', records);
+
+app.get('/post', (req, res) => {
+	fs.readFile('post.html', (err, data) => {
+		res.writeHead(200, {'Content-Type':'text/html'});
+		res.end(data);
+	});
+});
+app.get('/login', (req, res) => {
+	fs.readFile('login.html', (err, data) => {
+		res.writeHead(200, {'Content-Type':'text/html'});
+		res.end(data);
+	});
+});
+app.listen(30001, () => {
+    console.log('start');
+})
